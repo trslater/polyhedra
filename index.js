22e54ad273adbec57b1c5ae5e3fc0ee85c2853fc
Tristan Slater <hello@trslater.ca>
2022-06-10 13:38:16 -0700

    Set up scene, camera, orbital controls, and simple test mesh

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-import { Scene } from 'three'
+import { Scene, PerspectiveCamera, BoxGeometry, MeshNormalMaterial, Mesh, WebGLRenderer } from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+
+const windowAspect = window.innerWidth/window.innerHeight
+
+// 3D Environment
+// ----------------------------------------------------------------------------
 
 const scene = new Scene()
+const camera = new PerspectiveCamera(75, windowAspect, 0.1, 1000)
+const renderer = new WebGLRenderer()
+const controls = new OrbitControls(camera, renderer.domElement)
+const geometry = new BoxGeometry(2, 2, 2, 2)
+const material = new MeshNormalMaterial()
+const cube = new Mesh(geometry, material)
 
-console.log(scene)
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+scene.add(cube)
+
+camera.position.z = 5
+
+function animate() {
+    controls.update()
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
+
+// Entrypoint
+// ----------------------------------------------------------------------------
+
+animate()
