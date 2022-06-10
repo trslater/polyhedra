24d128f4c1ecf37b66c472ab53f3379cbab36c3b
Tristan Slater <hello@trslater.ca>
2022-06-10 13:40:06 -0700

    Add sample GUI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Scene, PerspectiveCamera, BoxGeometry, MeshNormalMaterial, Mesh, WebGLRenderer } from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { GUI } from "dat.gui"
 
 const windowAspect = window.innerWidth/window.innerHeight
 
@@ -27,6 +28,15 @@ function animate() {
 	requestAnimationFrame(animate)
 	renderer.render(scene, camera)
 }
+
+// GUI
+// ----------------------------------------------------------------------------
+
+const gui = new GUI()
+
+gui.add(cube.scale, "x", 0, 2).name("x Scale")
+gui.add(cube.scale, "y", 0, 2).name("y Scale")
+gui.add(cube.scale, "z", 0, 2).name("z Scale")
 
 // Entrypoint
 // ----------------------------------------------------------------------------
